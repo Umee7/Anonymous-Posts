9af552d14f240746a6715a661c4d15890dc3ed33
Harkishan Khuva <78949167+hakiKhuva@users.noreply.github.com>
2023-08-19 10:43:29 +0530

    Update triggers.sql

--- supabase-data/queries/triggers.sql
+++ supabase-data/queries/triggers.sql
@@ -1,9 +1,8 @@
-drop trigger add_request_data_on_post_insert on posts;
 create trigger add_request_data_on_post_insert after insert on posts
 for each row
 execute function insert_the_request();
 
-drop trigger add_request_data_on_comment_insert on comments;
+
 create trigger add_request_data_on_comment_insert after insert on comments
 for each row
 execute function insert_the_request();
