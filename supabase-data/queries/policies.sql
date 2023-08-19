6774061419b26e0d40fce1b7e017847c69b670c7
Harkishan Khuva <78949167+hakiKhuva@users.noreply.github.com>
2023-08-19 10:44:13 +0530

    Update policies.sql

--- supabase-data/queries/policies.sql
+++ supabase-data/queries/policies.sql
@@ -1,46 +1,39 @@
-DROP POLICY "Allow to get countries" ON "public"."countries";
 CREATE POLICY "Allow to get countries" ON "public"."countries"
 AS PERMISSIVE FOR SELECT
 TO anon
 USING (get_header('origin') = get_running_origin());
 
 
-DROP POLICY "Allow to get categories" ON "public"."categories";
 CREATE POLICY "Allow to get categories" ON "public"."categories"
 AS PERMISSIVE FOR select
 TO anon
 USING (get_header('origin') = get_running_origin());
 
 
-DROP POLICY "Allow users to Insert posts" ON "public"."posts";
 CREATE POLICY "Allow users to Insert posts" ON "public"."posts"
 AS PERMISSIVE FOR INSERT
 To anon
 WITH CHECK ((get_user_requests_count() < 8) AND (get_header('origin') = get_running_origin()));
 
 
-DROP POLICY "Allow users to select posts" ON "public"."posts";
 CREATE POLICY "Allow users to select posts" ON "public"."posts"
 AS PERMISSIVE FOR SELECT
 To anon
 USING (get_header('origin') = get_running_origin());
 
 
-DROP POLICY "Allow users to select comments" ON "public"."comments";
 CREATE POLICY "Allow users to select comments" ON "public"."comments"
 AS PERMISSIVE FOR select
 To anon
 USING (get_header('origin') = get_running_origin());
 
 
-DROP POLICY "Allow users to insert comments" ON "public"."comments";
 CREATE POLICY "Allow users to insert comments" ON "public"."comments"
 AS PERMISSIVE FOR INSERT
 To anon
 WITH CHECK ((get_user_requests_count() < 15) AND (get_header('origin') = get_running_origin()));
 
 
-DROP POLICY "Allow admin to do all" ON "public"."user_requests";
 CREATE POLICY "Allow admin to do all" ON "public"."user_requests"
 AS PERMISSIVE FOR INSERT
 TO anon
