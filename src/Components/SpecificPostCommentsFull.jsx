48fd294b1a2744ff55f9ddb56e7de95864d82a08
Harkishan Khuva <78949167+hakiKhuva@users.noreply.github.com>
2023-09-02 23:26:08 +0530

    removed +1 counting from comments

--- src/Components/SpecificPostCommentsFull.jsx
+++ src/Components/SpecificPostCommentsFull.jsx
@@ -11,7 +11,7 @@ const SpecificPostCommentFull = observer(((props) => {
 
     return (
     <div>
-        <h2 className="p-2 text-xl">Comments<span className="text-sm mx-1">({formatNumber(props.comments_count+state.frozenComments.length)})</span></h2>
+        <h2 className="p-2 text-xl">Comments<span className="text-sm mx-1">({formatNumber(props.comments_count)})</span></h2>
         <SpecificPostCommentBox referenceid={props.reference_id} addNote={props.addNote} addComment={(comment)=>{
             state.addFrozenComment(comment)
             props.updateCommentCount(state.frozenComments.length)
