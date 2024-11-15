5dac38e056b5f22278b2596d62c60f117064298f
Umee7 <umeshmessi23@gmail.com>
2024-11-16 04:17:09 +0530

    initial commit

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ const Header = ({currentTheme, updateTheme}) => {
     return(
         <header className="dark:bg-cyan-300 dark:text-black font-bold bg-neutral-800 text-white z-50">
             <div className="max-w-frame mx-auto flex items-center justify-between">
-                <h1><Link to={"/"} className="block p-5 text-lg text-center mx-2">Anon posts</Link></h1>
+                <h1><Link to={"/"} className="block p-5 text-lg text-center mx-2">Anonymous posts</Link></h1>
                 <button type="button" className="border-2 dark:border-black p-1 text-xl rounded-full mx-2" onClick={updateTheme}>
                     {
                         currentTheme === 'light'?
--- src/supabase.js
+++ src/supabase.js
@@ -4,4 +4,9 @@ const supabase = createClient(
     import.meta.env.VITE_SUPABASE_URL,
     import.meta.env.VITE_SUPABASE_KEY
 )
+
+// import { createClient } from '@supabase/supabase-js'
+// const supabaseUrl = 'https://ewwdirutpdvcddqtmaof.supabase.co'
+// const supabaseKey = process.env.SUPABASE_KEY
+// const supabase = createClient(supabaseUrl, supabaseKey)
 export {supabase}
